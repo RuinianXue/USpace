c7d0feab624ba15caf15d7469e70db8dec389b75
karen327 <2857809611@qq.com>
2023-11-08 10:54:54 +0800

    Modify the error of initial page

--- ToDo/Properties/Settings.Designer.cs
+++ ToDo/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LinkToDo.Properties
+namespace ToDo.Properties
 {
 
 
--- ToDo/Scripts/MysqlBase.cs
+++ ToDo/Scripts/MysqlBase.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LinkToDo.Myscripts
+namespace ToDo.Myscripts
 {
     internal class MysqlBase
     {
