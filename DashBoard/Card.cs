0515e83223fb1396f7eb50b928573e2d76a71dcc
RuinianXue <92255709+RuinianXue@users.noreply.github.com>
2023-11-09 14:26:00 +0800

    new_merge

---
+++ DashBoard/Card.cs
@@ -0,0 +1,67 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace DashBoard
+{
+    public class Card : MaterialDesignThemes.Wpf.Card
+    {
+        public Card()
+        {
+            // Set default values
+            //ShadowAssist.SetShadowDepth(this, 0);
+            UniformCornerRadius = 15;
+            BorderThickness = new Thickness(5);
+            BorderBrush = Brushes.White;
+            Width = Constants.SQUARE_LENGTH;
+            Height = Constants.SQUARE_LENGTH;
+            Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#F6F6F8"));
+
+
+            // Create the StackPanel
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Margin = new Thickness(10);
+
+            // Create the PackIcon
+            PackIcon packIcon = new PackIcon();
+            packIcon.Kind = PackIconKind.Twitter;
+            packIcon.Foreground = Brushes.LightBlue;
+            stackPanel.Children.Add(packIcon);
+
+            // Create the TextBlock for followers count
+            TextBlock followersTextBlock = new TextBlock();
+            followersTextBlock.FontWeight = FontWeights.SemiBold;
+            followersTextBlock.FontSize = 25;
+            followersTextBlock.Text = "280K";
+            followersTextBlock.Margin = new Thickness(0, 10, 0, 0);
+            stackPanel.Children.Add(followersTextBlock);
+
+            // Create the TextBlock for "Followers" label
+            TextBlock label = new TextBlock();
+            label.FontSize = 12;
+            label.Text = "Followers";
+            stackPanel.Children.Add(label);
+
+            // Create the PackIcon for ellipsis
+            PackIcon ellipsisIcon = new PackIcon();
+            ellipsisIcon.Kind = PackIconKind.EllipsisHorizontal;
+            ellipsisIcon.HorizontalAlignment = HorizontalAlignment.Right;
+            stackPanel.Children.Add(ellipsisIcon);
+
+            // Set the Content of the Card to the StackPanel
+            Content = stackPanel;
+        }
+        public void SetPosition(Grid grid, int row,int colomn)
+        {
+            Grid.SetRow(this, row);
+            Grid.SetColumn(this, colomn);
+            grid.Children.Add(this);
+        }
+    }
+}
---
+++ DashBoard/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashBoard
+{
+    public static class Constants
+    {
+        public const int SQUARE_LENGTH = 187;
+        public const int BIG_SQUARE = 200;
+        public const int EDGE = 20;
+        public const int MAX_ROW = 4;
+        public const int MAX_COLOMN = 5;
+        public const int INSIDE_HEIGHT = MAX_ROW * BIG_SQUARE + 4 * EDGE;
+        public const int INSIDE_WIDTH = MAX_COLOMN * BIG_SQUARE + 4 * EDGE;
+    }
+}
